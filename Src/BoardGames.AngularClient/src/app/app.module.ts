5e1faace43180093db3f6c3afa2ae6a29f97a556
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2023-05-25 00:20:29 +0200

    added empty home component

--- Src/BoardGames.AngularClient/src/app/app.module.ts
+++ Src/BoardGames.AngularClient/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-@NgModule({
+@NgModule( {
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( [
+      { path: 'home', loadChildren: () => import( './home/home.module' ).then( m => m.HomeModule ) }
+    ] )
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
+} )
 export class AppModule { }
