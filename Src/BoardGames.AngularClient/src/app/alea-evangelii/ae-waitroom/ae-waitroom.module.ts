6ca42cbf6c8016d70820d6718d9425abfc930bbd
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2023-05-27 20:15:14 +0200

    styles for waitroom; added rules card

--- Src/BoardGames.AngularClient/src/app/alea-evangelii/ae-waitroom/ae-waitroom.module.ts
+++ Src/BoardGames.AngularClient/src/app/alea-evangelii/ae-waitroom/ae-waitroom.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AeRoomSelectorModule } from '../ae-room-selector/ae-room-selector.module';
 import { AeWaitroomComponent } from './ae-waitroom.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 const routes: Routes = [
@@ -16,7 +17,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild( routes ),
-    AeRoomSelectorModule
+    AeRoomSelectorModule,
+    NgbCarouselModule
   ]
 } )
 export class AeWaitroomModule { }
