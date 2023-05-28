9e9faeadf1a14b88048f0863d34c16e62b4ca765
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2023-05-28 22:50:33 +0200

    added cors

--- Src/BoardGames.Server/Program.cs
+++ Src/BoardGames.Server/Program.cs
@@ -14,9 +14,27 @@ builder.Services
 
 builder.Services.AddSingleton<IAERoomStorage, AERoomStorage>();
 
+var corsOrigins = builder.Configuration
+			.GetValue<string>( "CorsOrigin" )
+			?.Split( ',' )
+			?? Array.Empty<string>();
+
+builder.Services.AddCors( o =>
+{
+	o.AddDefaultPolicy( p =>
+	{
+		p.WithOrigins( corsOrigins )
+			.AllowAnyMethod()
+			.AllowAnyHeader()
+			.AllowCredentials();
+	} );
+} );
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.MapHub<AleaEvangeliiHub>( "/realtime/alea-evangelii" );
 
